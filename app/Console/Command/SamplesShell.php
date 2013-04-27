676f2854b53684ff584af9df39cceed76d5610b1
Keijiro Senda <senda.keijiro@gmail.com>
2013-04-28 00:10:06 +0900

    Implemented SamplesShell::update().

--- app/Console/Command/SamplesShell.php
+++ app/Console/Command/SamplesShell.php
@@ -9,7 +9,7 @@ App::uses('Shell', 'Console');
  * @package       app.Console.Command
  */
 class SamplesShell extends AppShell {
-	public $uses = ['Sample', 'YouTube'];
+	public $uses = ['Sample', 'Tune', 'YouTube'];
 	public $tasks = ['TunesJson'];
 
 	public function main() {
@@ -23,17 +23,68 @@ USAGE;
 	}
 
 	public function update() {
-		$data = $this->TunesJson->load();
+		$source = $this->TunesJson->load();
 
-		$tunes = array_unique(Hash::extract($data, '{s}.tunes.{n}'));
+		$tunes = array_unique(Hash::extract($source, '{s}.tunes.{n}'));
 		sort($tunes);
 
-		var_dump($tunes);
+		foreach ($tunes as $tune) {
+			$this->out("'{$tune}'...");
 
-		// $samples = $this->YouTube->find('all', [
-		// 	'conditions' => ['q' => 'Miles Davis jazz',]
-		// ]);
+			$tune_id = $this->Tune->getIdByName($tune);
+			if (!$tune_id) {
+				$this->out('Not found in Tunes table. Ignored.');
+				continue;
+			}
 
+			$records = $this->YouTube->find('all', [
+				'conditions' => ['q' => "{$tune} jazz"]
+			]);
+
+			if (count($records['YouTube']) < 1) {
+				$this->out('No results hit on YouTube.');
+				continue;
+			} else {
+				if ($this->Sample->deleteAll(['Sample.tune_id' => $tune_id], false)) {
+					$this->out("Deleted Sample records where tune_id = {$tune_id}.");
+				}
+
+				foreach ($records['YouTube'] as $cnt => $record) {
+					$title = $record['title'];
+					$video_id = $record['videoId'];
+					$thumbnail_url = $record['thumbnailUrl'];
+
+					if (!$this->YouTube->downloadThumbnail(
+						$thumbnail_url,
+						$video_id
+					)) {
+						$this->out("Failed to download '{$thumbnail_url}'.");
+						continue;
+					}
+
+					$this->out("Downloaded thumbnail of {$tune}[{$cnt}] (videoId: {$video_id})...");
+
+					$this->Sample->create();
+					$sample_record = [
+						'Sample' => [
+							'tune_id' => $tune_id,
+							'title' => $title,
+							'type' => 'youtube',
+							'url' => $video_id,
+							'thumbnail' => "{$video_id}.jpg"
+						]
+					];
+					if (!$this->Sample->save($sample_record)) {
+						$this->out('Failed to save Sample record.');
+						continue;
+					}
+
+					$this->out("Sample.id = {$this->Sample->id} saved.");
+				}
+			}
+		}
+
+		$this->out('Done.');
 		return true;
 	}
 
